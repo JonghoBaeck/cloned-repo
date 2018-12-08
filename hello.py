76fb891be07cc32d8465f5fced840dd89e4ceabd
choiic <choic83@hotmail.com>
2018-12-08 09:18:59 +0000

    edit hello.py added one more print() and for statement.

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("Hello Python!")
+
+# Added by Choi.I.C.
+print("This is added line")
+for i in range(5):
+    if i%2==0:
+        print("{} is even".format(i))
+    else:
+        print("{} is odd".format(i))
